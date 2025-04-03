78f17b8e4bd4e137d2cc18ca90927077fbfaa2c9
Harshi <72039632+Harshi9843@users.noreply.github.com>
2025-04-03 19:57:43 +0100

    Added validation for username inputs

--- Menus/MainMenuManager.cs
+++ Menus/MainMenuManager.cs
@@ -6,14 +6,29 @@ using System;
 using System.Text;
 using System.Collections;
 using System.Xml.Serialization;
+using System.Text.RegularExpressions;
 
 public class MainMenuManager : MonoBehaviour
 {
     public GameObject namePanelPlay;
-    public GameObject namePanelRecord;
     public TMP_InputField enterName;
+    private string lastValidText;
+
+    public GameObject namePanelRecord;
     public TMP_InputField enterNameRecord;
+    private string lastValidText2;
+    
     private string playerName;
+    
+    
+
+    void Start()
+    {
+        enterName.characterLimit = 12;
+        enterNameRecord.characterLimit = 12;
+        enterName.onValueChanged.AddListener(ValidateInput);
+        enterNameRecord.onValueChanged.AddListener(ValidateInput2);
+    }
 
     public void Play(){
         namePanelPlay.SetActive(true);
@@ -42,5 +57,20 @@ public class MainMenuManager : MonoBehaviour
         PlayerPrefs.SetString("playerName", playerName);
         playerName = PlayerPrefs.GetString("playerName");
         SceneManager.LoadScene("Player Records");
+    }
+
+    public void ValidateInput(string text){
+        if(Regex.IsMatch(text, @"[^a-zA-Z\s]")){
+            enterName.text = lastValidText;
+        }else{
+            lastValidText = text;
+        }
+    }
+    public void ValidateInput2(string text){
+        if(Regex.IsMatch(text, @"[^a-zA-Z\s]")){
+            enterNameRecord.text = lastValidText2;
+        }else{
+            lastValidText2 = text;
+        }
     }
 }
